358f8795101fd4b442a5946a5cf0cfa1cb99bce9
Biju S Nair <bijusalapatt@gmail.com>
2018-04-09 21:01:09 +0530

    List Sample added

---
+++ ListSample/List01.py
@@ -0,0 +1,12 @@
+Students=['Renjith','Binu','Venu','Joice']
+
+print(Students[0])  # //Renjith
+
+print(Students[-1]) # Last one - Joice
+
+# Slicing a List
+print(Students[:1]) # ['Renjith']
+print(Students[:2]) # ['Renjith', 'Binu']
+
+print(Students[1:2]) # ['Binu']
+print(Students[2:]) # ['Venu', 'Joice']
